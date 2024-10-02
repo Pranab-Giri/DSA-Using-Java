c420fb3ea0ccab7cd813ba612e5fd9cab3252f1f
Pranab Giri <140830008+Pranab-Giri@users.noreply.github.com>
2024-10-02 09:55:36 +0530

    Time: 0 ms (100%), Space: 56.7 MB (5.01%) - LeetHub

--- 0882-peak-index-in-a-mountain-array/0882-peak-index-in-a-mountain-array.java
+++ 0882-peak-index-in-a-mountain-array/0882-peak-index-in-a-mountain-array.java
@@ -1,15 +1,35 @@
 import java.util.*;
 class Solution {
     public int peakIndexInMountainArray(int[] arr) {
-        int maxEle = 0;
-        int peek = 0;
-        for(int i=0; i<arr.length; i++){
-            if(maxEle < arr[i]){
-                maxEle = arr[i];
-                peek = i;
+        /* Brute force Approach */
+        // int maxEle = 0;
+        // int peek = 0;
+        // for(int i=0; i<arr.length; i++){
+        //     if(maxEle < arr[i]){
+        //         maxEle = arr[i];
+        //         peek = i;
+        //     }
+        // }
+        // return peek;
+
+        /* optimal  Approach(Binary Search) */
+         int left = 0, right = arr.length - 1;
+
+        while (left < right) {
+            int mid = left + (right - left) / 2; // Calculate mid point
+            
+            // Check if we're in the increasing or decreasing part of the mountain
+            if (arr[mid] < arr[mid + 1]) {
+                // If mid is less than its next element, we're in the increasing part
+                left = mid + 1;
+            } else {
+                // If mid is greater than or equal to its next element, we're in the decreasing part
+                right = mid;
             }
         }
-        return peek;
+
+        // When left == right, we've found the peak index
+        return left;
         
     }
 }
