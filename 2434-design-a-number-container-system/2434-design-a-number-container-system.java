d0e65f23e4b19873ea33c145bce1dd5fb81a400e
Pranab Giri <140830008+Pranab-Giri@users.noreply.github.com>
2025-02-08 21:24:33 +0530

    Time: 84 ms (47.95%), Space: 98.9 MB (56.85%) - LeetHub

--- 2434-design-a-number-container-system/2434-design-a-number-container-system.java
+++ 2434-design-a-number-container-system/2434-design-a-number-container-system.java
@@ -1,49 +1,35 @@
 import java.util.*;
 
 class NumberContainers {
-    private Map<Integer, Integer> indexToNumber;  // Maps index to number
-    private Map<Integer, PriorityQueue<Integer>> numberToIndices; // Maps number to min heap of indices
+    private Map<Integer, Integer> indexToNumber;
+    private Map<Integer, TreeSet<Integer>> numberToIndices;
 
     public NumberContainers() {
         indexToNumber = new HashMap<>();
         numberToIndices = new HashMap<>();
     }
-    
+
     public void change(int index, int number) {
         if (indexToNumber.containsKey(index)) {
             int oldNumber = indexToNumber.get(index);
-            if (oldNumber != number) {
-                numberToIndices.get(oldNumber).remove(index); // Remove old index
+            if (numberToIndices.containsKey(oldNumber)) {
+                numberToIndices.get(oldNumber).remove(index);
+                if (numberToIndices.get(oldNumber).isEmpty()) {
+                    numberToIndices.remove(oldNumber);
+                }
             }
         }
         
-        indexToNumber.put(index, number); // Update index to new number
-        numberToIndices.putIfAbsent(number, new PriorityQueue<>());
-        numberToIndices.get(number).offer(index);
+        indexToNumber.put(index, number);
+        numberToIndices.putIfAbsent(number, new TreeSet<>());
+        numberToIndices.get(number).add(index);
     }
-    
+
     public int find(int number) {
-        if (!numberToIndices.containsKey(number) || numberToIndices.get(number).isEmpty()) {
-            return -1;
+        if (numberToIndices.containsKey(number) && !numberToIndices.get(number).isEmpty()) {
+            return numberToIndices.get(number).first();
         }
-
-        PriorityQueue<Integer> minHeap = numberToIndices.get(number);
-
-        // Remove stale indices (ensuring index-to-number consistency)
-        while (!minHeap.isEmpty() && indexToNumber.get(minHeap.peek()) != number) {
-            minHeap.poll();
-        }
-
-        return minHeap.isEmpty() ? -1 : minHeap.peek();
+        return -1;
     }
-
-   
 }
 
-
-/**
- * Your NumberContainers object will be instantiated and called as such:
- * NumberContainers obj = new NumberContainers();
- * obj.change(index,number);
- * int param_2 = obj.find(number);
- */
